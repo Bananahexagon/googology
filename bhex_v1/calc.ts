703d1a25cbfd5f2af2ffd4e9d610717bebd3a2f6
bananahexagon <obsidian.bananafool@gmail.com>
2024-02-04 14:40:18 +0900

    #

--- bhex_v1/calc.ts
+++
@@ -1,5 +0,0 @@
-import { AST, zero, one, omega, mahlo } from "./define.js"
-
-export function calc(s: AST, t: AST):string {
-    return "";
-};
--- bhex_v1/main.js
+++
@@ -1,9 +0,0 @@
-import { parse } from "./parser.js";
-var input = document.querySelector("#input");
-var button = document.querySelector("#button");
-var output = document.querySelector("#result");
-button.addEventListener("click", function () { return main(input.value); });
-function main(input) {
-    var ast = parse(input);
-    console.log(ast);
-}
